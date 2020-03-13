86087cbdfd7f3cf962b323c6a622cd36577b06f9
sametweb <sametmutevellioglu@gmail.com>
2020-03-12 20:21:03 -0700

    Routers applied, files separated

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const hubsRouter = require("./hubs/hubs-router");
+
+const server = express();
+
+server.use(express.json());
+server.use("/api/hubs", hubsRouter);
+
+//endpoints
+
+server.get("/", (req, res) => {
+  res.send(`
+          <h2>Lambda Hubs API</h>
+          <p>Welcome to the Lambda Hubs API</p>
+        `);
+});
+
+module.exports = server;
